61e54c3d28addba98dbdd646ab162711788cda15
Emma Fredriksen <emmafre@stud.ntnu.no>
2026-06-27 11:33:44 +0200

    chore: add tailwindcss

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
 export default function HomePage(){
 
     return(
-        <h1>EMMIZ</h1>
+        <div className="home-container">
+            <h1>EMMIZ</h1>
+            <button>
+                <img src="play.svg" alt="play icon" />
+            </button>
+        </div>
     );
 
 }
