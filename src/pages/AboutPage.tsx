11af306982b3358df11d7ac5014af5e1995a987c
Emma Fredriksen <emmafre@stud.ntnu.no>
2026-08-16 00:43:53 +0200

    feat: update about me page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -4,13 +4,20 @@ export default function AboutPage(){
             <div className="about flex flex-col md:flex-row min-h-screen w-full">
                 <div className="left flex-1 m-8 md:m-20">
                     <h1 style={{color: "var(--current-color)"}}>About me</h1>
-                    <p className="py-2 md:py-10 text-black text-sm md:text-xl">I’m a 21-year-old Computer Science student passionate about creating solutions that make a real impact. I especially enjoy building projects that are both fun and useful.
-                        I chose this path because I love seeing my creations in action and knowing they make a difference for others.
+                    <p className="py-2 md:py-10 text-black text-sm md:text-xl">
+                        My name is Emma Fredriksen, and I am a computer science student at NTNU. 
+                        I am currently in my third year and will complete my bachelor’s degree this spring!
+                        Alongside my studies, I am actively involved in student volunteering. 
+                        I currently serve as the leader of "Applikasjonskomiteen", a technical committee within Online, the student association for computer science students at NTNU.
                     </p>
                     <br />
-                    <p className="text-black text-sm md:text-xl">My main interests are in web development and user-centered design, where I can combine problem-solving with creativity. I’m constantly exploring new technologies and refining my skills through both academic projects and personal initiatives.</p>
-                    <button className="md:w-28 p-3 mt-8 md:mt-16 rounded-3xl" style={{background: "var(--current-color)"}}>Instagram</button>
-                    <button className="md:w-28 p-3 mt-8 md:mt-16 ml-5 rounded-3xl" style={{background: "var(--current-color)"}}>Facebook</button>
+                    <p className="text-black text-sm md:text-xl">
+                        I have participated in several group projects as part of various courses at NTNU and have a few hobby projects of my own.
+                    </p>
+                    <div className="mt-8 md:mt-16">
+                        <a href="https://appkom.no/" className="md:w-28 px-4 py-3 rounded-3xl" style={{background: "var(--current-color)"}}>Appkom</a>
+                        <a href="#newproject" className="md:w-28 ml-5 px-4 py-3 mt-8 md:mt-16 rounded-3xl" style={{background: "var(--current-color)"}}>Projects</a>
+                    </div>
 
                 </div>
                 <div className="right flex md:flex-1 justify-center items-center">
