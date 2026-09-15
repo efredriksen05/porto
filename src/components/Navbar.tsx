b36de1e03f89b28be8d2d4fc2810eb3c40423a61
Emma Fredriksen <emmafre@stud.ntnu.no>
2026-09-15 22:50:33 +0200

    feat: hide navbar on small screens
    The navbar is unpractical on smaller screens, will therefore hide it and replace it with a hamburger menu later

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Star } from 'lucide-react';
 
 export default function Navbar (){
     return (
-        <div className="text-white flex flex-row items-center justify-center gap-10 mt-10 absolute left-0 right-0 z-20">
+        <div className="text-white md:flex flex-row hidden md:visible items-center justify-center gap-10 mt-10 absolute left-0 right-0 z-20">
             <a href="">Skills</a>
             <a href="#about">About me</a>
             <Star size={24} fill="white"/>
